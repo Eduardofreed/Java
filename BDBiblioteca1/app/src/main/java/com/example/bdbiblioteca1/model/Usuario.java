e74f4ac8434b05efa1ea078dbecf6bd9b9b2c513
Eduardo da Silva Lopes <160648486+Eduardofreed@users.noreply.github.com>
2025-02-26 09:59:35 -0300

    Update Usuario.java

--- BDBiblioteca1/app/src/main/java/com/example/bdbiblioteca1/model/Usuario.java
+++ BDBiblioteca1/app/src/main/java/com/example/bdbiblioteca1/model/Usuario.java
@@ -2,6 +2,39 @@ package com.example.bdbiblioteca1.model;
 
 public class Usuario {
     private String nome;
-    private int dt_nascimento;
+    private int senha;
+    private String nivel;
+
+    public Usuario(String nome, int senha, String nivel) {
+        this.nome = nome;
+        this.senha = senha;
+        this.nivel = nivel;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private int getSenha() {
+        return senha;
+    }
+
+    private void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+    public void selecionar(){
+        System.out.println("Conteúdo selecionado");
+    }
 }
 
